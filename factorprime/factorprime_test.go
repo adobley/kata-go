9861c75646b565a7e0fc2985953bc43b69f89c10
Aidan Obley <aobley@thoughtworks.com>
2017-02-05 18:46:06 -0500

    Tests for empty list when factoring 1

--- factorprime/factorprime_test.go
+++ factorprime/factorprime_test.go
@@ -5,5 +5,8 @@ import "testing"
 func TestFactorPrime(t *testing.T) {
 	t.Log("Returns an empty list when factoring one")
 	result := Factor(1)
-
+	if len(result) != 0 {
+		t.Errorf("Did not return an empty list!\n"+
+			"Actual: %d", result)
+	}
 }
