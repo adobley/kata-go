f20cf80bc2f7c713182bc61980b0c8fc17e937d0
Aidan Obley <aobley@thoughtworks.com>
2017-02-08 19:45:07 -0500

    Cleaned up tests a bit
    Moved all tests for a given function into a single test function.
    Go doesn't throw an exception out of the test entirely on failure, so why not?
    It looks cleaner, but produces a more verbose error, showing all of the log messages for the test.
    I'm torn on how I feel about this. I'll leave it alone for now.
    
    I inlined all the result references because they weren't doing much.

--- factorprime/factorprime_test.go
+++ factorprime/factorprime_test.go
@@ -2,58 +2,33 @@ package factorprime
 
 import "testing"
 
-func Test_FactorPrime_ReturnsEmptySlice_WhenFactoringOne(t *testing.T) {
+func TestFactorPrime(t *testing.T) {
 	t.Log("Returns [] when factoring 1")
-	result := Factor(1)
-	assertSlicesEqual(t, []int{}, result)
-}
+	assertSlicesEqual(t, []int{}, Factor(1))
 
-func Test_FactorPrime_ReturnsSliceWithTwo_WhenFactoringTwo(t *testing.T) {
 	t.Log("Returns [2] when factoring 2")
-	result := Factor(2)
-	assertSlicesEqual(t, []int{2}, result)
-}
+	assertSlicesEqual(t, []int{2}, Factor(2))
 
-func Test_FactorPrime_ReturnsSliceWithThree_WhenFactoringThree(t *testing.T) {
 	t.Log("Returns [3] when factoring 3")
-	result := Factor(3)
-	assertSlicesEqual(t, []int{3}, result)
-}
+	assertSlicesEqual(t, []int{3}, Factor(3))
 
-func Test_FactorPrime_ReturnsSliceWithTwoTwos_WhenFactoringFour(t *testing.T) {
 	t.Log("Returns [2, 2] when factoring 4")
-	result := Factor(4)
-	assertSlicesEqual(t, []int{2, 2}, result)
-}
+	assertSlicesEqual(t, []int{2, 2}, Factor(4))
 
-func Test_FactorPrime_ReturnsSliceWithFive_WhenFactoringFive(t *testing.T) {
 	t.Log("Returns [5] when factoring 5")
-	result := Factor(5)
-	assertSlicesEqual(t, []int{5}, result)
-}
+	assertSlicesEqual(t, []int{5}, Factor(5))
 
-func Test_FactorPrime_ReturnsSliceWithTwoAndThree_WhenFactoringSix(t *testing.T) {
 	t.Log("Returns [2, 3] when factoring 6")
-	result := Factor(6)
-	assertSlicesEqual(t, []int{2, 3}, result)
-}
+	assertSlicesEqual(t, []int{2, 3}, Factor(6))
 
-func Test_FactorPrime_ReturnsSliceWithSeven_WhenFactoringSeven(t *testing.T) {
 	t.Log("Returns [7] when factoring 7")
-	result := Factor(7)
-	assertSlicesEqual(t, []int{7}, result)
-}
+	assertSlicesEqual(t, []int{7}, Factor(7))
 
-func Test_FactorPrime_ReturnsSliceWithThreeTwos_WhenFactoringEight(t *testing.T) {
 	t.Log("Returns [2, 2, 2] when factoring 8")
-	result := Factor(8)
-	assertSlicesEqual(t, []int{2, 2, 2}, result)
-}
+	assertSlicesEqual(t, []int{2, 2, 2}, Factor(8))
 
-func Test_FactorPrime_ReturnsSliceWithCorrectPrimes_WhenFactoringLargeNumber(t *testing.T) {
 	t.Log("Returns [2, 2, 2, 3, 5, 5, 7, 11, 13, 17, 17] when factoring 2 * 2 * 2 * 3 * 5 * 5 * 7 * 11 * 13 * 17 * 17")
-	result := Factor(2 * 2 * 2 * 3 * 5 * 5 * 7 * 11 * 13 * 17 * 17)
-	assertSlicesEqual(t, []int{2, 2, 2, 3, 5, 5, 7, 11, 13, 17, 17}, result)
+	assertSlicesEqual(t, []int{2, 2, 2, 3, 5, 5, 7, 11, 13, 17, 17}, Factor(2*2*2*3*5*5*7*11*13*17*17))
 }
 
 func assertSlicesEqual(t *testing.T, expected, actual []int) {
@@ -72,8 +47,8 @@ func assertSlicesEqual(t *testing.T, expected, actual []int) {
 
 func errorSlicesNotEqual(t *testing.T, expected, actual []int) {
 	t.Errorf("Did not return expected slice:\n"+
-		"Expected: %d\n"+
-		"Actual: %d",
+		"\t\tExpected: %d\n"+
+		"\t\tActual: %d",
 		expected,
 		actual)
 }
